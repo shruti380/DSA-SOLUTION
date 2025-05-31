1aaaf1b839e6424dd2048d6eaafdc13b597f57e1
Shruti Gupta <shrutigupta2987@gmail.com>
2025-05-31 12:00:37 +0530

    Time: 17 ms (62.10%) | Memory: 28.2 MB (60.48%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int miniCoins(int ind,vector<int>&coins,int amount,vector<vector<int>>&dp){
+        if(ind==0){
+            if(amount%coins[0]==0){
+              return  1;
+            }
+            else{
+                return 0;
+            }
+        }
+        if(dp[ind][amount]!=-1){
+            return dp[ind][amount];
+        }
+        int notTake=miniCoins(ind-1,coins,amount,dp);
+        int take=0;
+        if(coins[ind]<=amount){
+            take=miniCoins(ind,coins,amount-coins[ind],dp);
+        }
+        return dp[ind][amount]=(take+notTake);
+}
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return miniCoins(n-1,coins,amount,dp);
+        
+    }
+};
