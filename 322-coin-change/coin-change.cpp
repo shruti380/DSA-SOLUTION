f52a84b0ff9bed4159d6a15857f60eaa61ce4fd9
Shruti Gupta <shrutigupta2987@gmail.com>
2025-05-31 11:34:08 +0530

    Time: 47 ms (34.44%) | Memory: 48.9 MB (34.64%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-int miniCoins(int ind,vector<int>&coins,int amount,vector<vector<int>>&dp){
-        if(ind==0){
-            if(amount%coins[0]==0){
-              return  ( amount/coins[0]);
-            }
-            else{
-                return 1e9;
-            }
-        }
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
-        }
-        int notTake=0+miniCoins(ind-1,coins,amount,dp);
-        int take=1e9;
-        if(coins[ind]<=amount){
-            take=1+miniCoins(ind,coins,amount-coins[ind],dp);
-        }
-        return dp[ind][amount]=min(take,notTake);
-}
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int result= miniCoins(n-1,coins,amount,dp);
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0){
+                dp[0][t]=t/coins[0];
+            }
+            else{
+                dp[0][t]=1e9;         
+           }
+        }
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int notTake=0+dp[i-1][t];
+        int take=1e9;
+        if(coins[i]<=t){
+            take=1+dp[i][t-coins[i]];
+        }
+        dp[i][t]=min(take,notTake);
+
+            }
+        }
+        int result=dp[n-1][amount];
         if(result>=1e9){
             return -1;
         }
         return result;
+
         
     }
 };
