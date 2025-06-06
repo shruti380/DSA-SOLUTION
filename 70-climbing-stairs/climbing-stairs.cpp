f918b0d5d4b78fa0d036fea091df52d39036dda2
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-06 19:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -10,6 +10,11 @@ int memo(int ind,vector<int>&dp){
 }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return memo(n,dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
