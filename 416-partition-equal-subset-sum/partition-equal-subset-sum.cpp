cf29f25ba243cbbcc092dd9fb9e467085a050479
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-07 12:25:51 +0530

    Time: 146 ms (30.75%) | Memory: 17.5 MB (46.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        int totalSum=0;
+        int sum=0;
         for(int i=0;i<n;i++){
-            totalSum+=nums[i];
+           sum+=nums[i];
         }
-        if(totalSum%2!=0){
+        if(sum%2!=0){
             return false;
         }
-        else{
-            int k=totalSum/2;
-            vector<vector<bool>>dp(n,vector<bool>(k+1));
-            for(int i=0;i<n;i++){
-                dp[i][0]=true;
-            }
-            if(nums[0]<=k){
-                dp[0][nums[0]]=true;
-            }
-            for(int i=1;i<n;i++){
-                for(int sum=1;sum<=k;sum++){
-                    bool notTaken=dp[i-1][sum];
-                    bool taken=false;
-                    if(nums[i]<=sum){
-                        taken=dp[i-1][sum-nums[i]];
-                    }
-                    dp[i][sum]=notTaken||taken;
-                }
-            }
-            return dp[n-1][k];
+        int k=sum/2;
+        vector<vector<bool>>dp(n+1,vector<bool>(k+1,false));
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
         }
-        
-        
-        
+        if(nums[0]<=k){
+            dp[0][nums[0]]=true;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int j=1;j<=k;j++){
+                bool notTaken=dp[ind-1][j];
+                bool taken=false;
+                if(nums[ind]<=j){
+                    taken=dp[ind-1][j-nums[ind]];
+                }
+                dp[ind][j]=taken||notTaken;
+
+            }
+
+        }
+        return dp[n-1][k];
     }
 };
