dbd4c6105c329b23b58588b521a52f4859d8c492
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-08 22:46:45 +0530

    Time: 3 ms (32.09%) | Memory: 12.7 MB (30.51%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,27 +1,24 @@
 class Solution {
-    void solve(vector<int>& nums, vector<vector<int>>& ans, int index, vector<int>& output) {
-        if (index >= nums.size()) {
-            ans.push_back(output);
-            return;
-        }
-
-        // Exclude current element
-        solve(nums, ans, index + 1, output);
-
-        // Include current element
-        output.push_back(nums[index]);
-        solve(nums, ans, index + 1, output);
-
-        // \U0001f519 Backtracking step (undo the last choice)
-        output.pop_back();
-    }
-
 public:
+void solve(int ind,vector<int>&nums,vector<vector<int>>&ans,vector<int>&output){
+    if(ind>=nums.size()){
+        ans.push_back(output);
+        return;
+    }
+    //  exclude
+    solve(ind+1,nums,ans,output);
+    int el=nums[ind];
+    output.push_back(el);
+    solve(ind+1,nums,ans,output);
+    output.pop_back();
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index = 0;
-        solve(nums, ans, index, output);
+        vector<int>output;
+        vector<vector<int>>ans;
+        int index=0;
+        solve(index,nums,ans,output);
         return ans;
+
+        
     }
 };
