989a0ec80b76b10548c5f7ef49c032c54d6402b7
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-10 23:50:56 +0530

    Time: 481 ms (64.83%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -5,3 +5,7 @@ FROM Weather w1
 INNER JOIN Weather w2
 ON DATEDIFF(w1.recordDate,w2.recordDate)=1
 AND w1.temperature>w2.temperature;
+
+
+-- WHERE SUBDATE(w2.recordDate,1)=w1.recordDate;
+-- Above line means that whenever we subtract the we date from 1 we should get w1.recordDate;
