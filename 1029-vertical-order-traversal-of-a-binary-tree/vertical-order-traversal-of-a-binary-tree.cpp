3134455e1d00ff1a0a4e8a52b7c9bb25c7c38726
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-17 15:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (52.19%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,88 +11,46 @@
  */
 class Solution {
 public:
-    // Function to perform vertical order traversal
-    // and return a 2D vector of node values
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // Handle null root case
-        if (!root) return {};
-        
-        // Map to store nodes based on
-        // vertical and level information
-        map<int, map<int, multiset<int>>> nodes;
-        
-        // Queue for BFS traversal, each
-        // element is a pair containing node
-        // and its vertical and level information
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        
-        // Push the root node with initial vertical
-        // and level values (0, 0)
-        todo.push({root, {0, 0}});
-        
-        // BFS traversal
-        while (!todo.empty()) {
-            // Retrieve the node and its vertical
-            // and level information from
-            // the front of the queue
-            auto p = todo.front();
+
+        if(!root){
+            return {};
+        }
+        map<int,map<int,multiset<int>>>nodes;
+        queue<pair<TreeNode*,pair<int,int>>>todo;
+
+        todo.push({root,{0,0}});
+
+        while(!todo.empty()){
+            auto p=todo.front();
             todo.pop();
-            TreeNode* temp = p.first;
-            
-            // Extract the vertical and level information
-            // x -> vertical
-            int x = p.second.first;
-            // y -> level
-            int y = p.second.second;
-            
-            // Insert the node value into the
-            // corresponding vertical and level
-            // in the map
+            TreeNode*temp=p.first;
+            int x=p.second.first;
+            int y=p.second.second;
+
             nodes[x][y].insert(temp->val);
-            
-            // Process left child
-            if (temp->left) {
+            if(temp->left){
                 todo.push({
                     temp->left,
                     {
-                        // Move left in
-                        // terms of vertical
-                        x - 1,
-                        // Move down in
-                        // terms of level
-                        y + 1
+                        x-1,y+1
                     }
                 });
             }
-            
-            // Process right child
-            if (temp->right) {
+            if(temp->right){
                 todo.push({
-                    temp->right,
-                    {
-                        // Move right in
-                        // terms of vertical
-                        x + 1,
-                        // Move down in
-                        // terms of level
-                        y + 1
+                    temp->right,{
+                        x+1,y+1
                     }
                 });
             }
         }
-        
-        // Prepare the final result vector
-        // by combining values from the map
-        vector<vector<int>> ans;
-        for (auto p : nodes) {
-            vector<int> col;
-            for (auto q : p.second) {
-                // Insert node values
-                // into the column vector
-                col.insert(col.end(), q.second.begin(), q.second.end());
+        vector<vector<int>>ans;
+        for(auto p:nodes){
+            vector<int>col;
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
             }
-            // Add the column vector
-            // to the final result
             ans.push_back(col);
         }
         return ans;
