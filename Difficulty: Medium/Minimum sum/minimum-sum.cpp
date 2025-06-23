0d36b7e5e1f39fcd5dd5eb4ba3637cbaef29b0b2
Shruti Gupta <shrutigupta2987@gmail.com>
2025-06-23 12:55:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -0,0 +1,59 @@
+class Solution {
+  public:
+  string addString(string&s1,string&s2){
+      string result="";
+      int i=s1.length()-1;
+      int j=s2.length()-1;
+      int c=0;
+      int sum=0;
+      while(i>=0||j>=0||c>0){
+          sum=c;
+          if(i>=0){
+              sum+=(s1[i]-'0');
+          }
+          if(j>=0){
+              sum+=(s2[j]-'0');
+          }
+          result.push_back(sum%10+'0');
+          c=sum/10;
+          i--;
+          j--;
+          
+          
+      }
+     while(!result.empty()&&result.back()=='0'){
+         result.pop_back();
+     }
+     reverse(result.begin(),result.end());
+     return result;
+  }
+    string minSum(vector<int> &arr) {
+        vector<int>count(10,0);
+        for(int num:arr){
+            count[num]++;
+        }
+        string s1="";
+        string s2="";
+        bool flag=true;
+        for(int digit=0;digit<10;digit++){
+            while(count[digit]--){
+                if(flag){
+                    if(!(s1.empty()&&digit==0)){
+                        s1.push_back(digit+'0');
+                    }
+                    
+                }
+                    else{
+                         if(!(s2.empty()&&digit==0)){
+                             s2.push_back(digit+'0');
+                         }
+                    }
+                    flag = !flag; 
+                }
+            }
+        
+        return addString(s1, s2);
+        
+        
+    }
+};
