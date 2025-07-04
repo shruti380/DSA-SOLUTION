c9bbc45fba5103b3c877ad313454ba933f317f4e
Shruti Gupta <shrutigupta2987@gmail.com>
2025-07-04 23:35:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,42 +1,37 @@
 // User Function Template
 class Solution {
   public:
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src){
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         // Code here
-        vector<vector<pair<int, int>>> adj(V);
-        for(auto& edge : edges) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+        vector<vector<pair<int,int>>>adj(V);
+        vector<int>dist(V,1e9);
+        for(auto edge:edges){
+            int u=edge[0],v=edge[1],w=edge[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
+         dist[src] = 0;
+        set<pair<int,int>>st;
+        st.insert({0,src});
+        while(!st.empty()){
+        auto it=*(st.begin());
+        int dis=it.first;
+        int node=it.second;
+        st.erase(it);
         
-        set<pair<int, int>> st;
-        vector<int> dist(V, 1e9);
-        
-        st.insert({0, src});
-        dist[src] = 0;
-        
-        while(!st.empty()) {
-            auto it = *(st.begin());
-            int node = it.second;
-            int dis = it.first;
-            st.erase(it);
-            
-            // Direct access to neighbors (much faster)
-            for(auto& neighbor : adj[node]) {
-                int adjNode = neighbor.first;
-                int edw = neighbor.second;
-                
-                if(dis + edw < dist[adjNode]) {
-                    if(dist[adjNode] != 1e9) {
-                        st.erase({dist[adjNode], adjNode});
-                    }
-                    dist[adjNode] = dis + edw;
-                    st.insert({dist[adjNode], adjNode});
+        for(auto neigh:adj[node]){
+            int adjNode=neigh.first;
+            int  edw=neigh.second;
+            if(edw+dis<dist[adjNode]){
+                if(dist[node]!=1e9){
+                    st.erase({dist[adjNode],adjNode});
                 }
+                dist[adjNode]=edw+dis;;
+                st.insert({dist[adjNode],adjNode});
             }
         }
+        }
         return dist;
+        
     }
 };
-    
