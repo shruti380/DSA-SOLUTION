2bffd7fed40ffa2c07958fefef514ffa8a0ed847
Shruti Gupta <shrutigupta2987@gmail.com>
2025-07-14 14:21:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (71.97%) - LeetSync

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result = 0;
+        ListNode* temp = head;
+        while(temp != nullptr) {
+            result = result * 2 + temp->val;
+            temp = temp->next;
+        }
+        return result;
+    }
+};
